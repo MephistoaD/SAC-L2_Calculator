85333a56ac3765a8bfd5f5f810c8c92279400cff
Lcarralde15 <lcarralde27@gmail.com>
2021-12-07 18:07:25 +0100

    Update Interpreter.java
    Cambio

--- code/src/main/java/cli/Interpreter.java
+++ code/src/main/java/cli/Interpreter.java
@@ -4,9 +4,11 @@ import valueobjects.InputValues;
 
 public class Interpreter implements CLIInterpreter{
     String data [] = new String[6];
+    System.out.printl("Hello");
 
     @Override
     public InputValues convertArguments(String[] args) {
+
         return null;
     }
 }
