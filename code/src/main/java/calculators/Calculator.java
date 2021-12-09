10cb616aa91f71b5ae02f8099c43af02d6ffa3bc
Lcarralde15 <lcarralde27@gmail.com>
2021-12-09 15:10:36 +0100

    Update Calculator.java
    AAL34 IMPLEMENTED

--- code/src/main/java/calculators/Calculator.java
+++ code/src/main/java/calculators/Calculator.java
@@ -19,10 +19,37 @@ public interface Calculator {
         return null; // new EthernetCalculator().calculate(bytes);
     }
     public static OutputValues calculateALL5ATM(int bytes ){
+
         return null; // new ALL5ATMCalculator().calculate(bytes);
     }
     public static OutputValues calculateALL34ATM(int bytes){
-        return null; // new ALL34ATMCalculator().calculate(bytes);
+            System.out.println("AAL34");
+            int numCeldas = bytes / 44;
+            if(bytes%44==0) {
+                numCeldas=(bytes/44);
+            }else if (bytes>44){
+                numCeldas=(n/44)+1;
+            }else {
+                numCeldas = 1;
+            }
+            int resto = bytes % 44;
+            int padding = 48 - 4 - resto;
+            int array[] = new int[3];
+            array[0] = numCeldas* 48;
+            array[1] = numCeldas;
+            array[2] = padding;
+            for(int i=0;i<3;i++) {
+                switch(i) {
+                    case 0: System.out.print("total bytes: ");
+                        break;
+                    case 1: System.out.print("total cells: ");
+                        break;
+                    case 2: System.out.print("padding: ");
+                        break;
+                }
+                System.out.println(array[i]);
+            }
+            return array;
     }
     OutputValues calculate(int bytes);
 }
