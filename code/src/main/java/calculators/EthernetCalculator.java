90c645a68cb5aed90d6e39cb148f3a4e4ef66dca
Lcarralde15 <lcarralde27@gmail.com>
2021-12-09 19:32:04 +0100

    Create EthernetCalculator.java

---
+++ code/src/main/java/calculators/EthernetCalculator.java
@@ -0,0 +1,12 @@
+package calculators;
+import valueobjects.InputValues;
+
+public class EthernetCalculator implements Calculator{
+
+
+    @Override
+    public OutputValues calculateEthernet (int bytes ) {
+
+    }
+}
+
