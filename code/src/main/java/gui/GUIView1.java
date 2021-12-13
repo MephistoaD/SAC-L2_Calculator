4d2abba0b8acdf82ddbe821056f6a40a14eb821a
danilostao <danilostao23@gmail.com>
2021-12-13 21:20:02 +0100

    Update GUIView1.java

--- code/src/main/java/gui/GUIView1.java
+++ code/src/main/java/gui/GUIView1.java
@@ -1,8 +1,10 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIView1 {
+public class GUIView1 extends JFrame{
     private JPanel panel1;
     private JTextField introduceTheBytesTextField;
     private JRadioButton AAL34RadioButton;
@@ -10,4 +12,65 @@ public class GUIView1 {
     private JRadioButton ethernetRadioButton;
     private JButton runButton;
     private JCheckBox paddingCheckBox;
+    ButtonGroup radiosGroup;
+    public String bytesText;
+    public GUIView1() {
+        JPanel jp = new JPanel();
+        jp.add(panel1);
+        JTextField jt= new JTextField();
+        jt.add(introduceTheBytesTextField);
+
+        //With these groups, we cannot select more than one button.
+        radiosGroup = new ButtonGroup();
+        radiosGroup.add(AAL34RadioButton);
+        radiosGroup.add(AAL5RadioButton);
+        radiosGroup.add(ethernetRadioButton);
+
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bytesText = introduceTheBytesTextField.getText(); //get the number of bytes that the users have wirte in the textbox
+                functionality(bytesText); //When you press the button "run" we will can see the option selected.
+            }
+        });
+    }
+    public void functionality(String bytesText){
+        //In this function we have three different "ifs" with the three algorithm that we have...
+        //if we select one of them we have two options, that we just have selected the algorithm, or we have selected the algorithm and the padding
+        String radioSelected="";
+        if(AAL34RadioButton.isSelected()){
+
+            if(paddingCheckBox.isSelected()){
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED but WITH PADDING
+            }else{
+                radioSelected+="AAL34 selected, ";
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED
+            }
+        }
+        if(AAL5RadioButton.isSelected()){
+
+            if(paddingCheckBox.isSelected()){
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED but WITH PADDING
+            }else {
+                radioSelected+="AAL5 selected, ";
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED
+            }
+        }
+        if(ethernetRadioButton.isSelected()){
+
+            if(paddingCheckBox.isSelected()){
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED but WITH PADDING
+            }else {
+                radioSelected+="Ethernet selected, ";
+                //HERE WE CALL THE FUNCTION WHICH CALCULATE THE ALGORITHM SELECTED
+            }
+        }
+        JOptionPane.showMessageDialog(null, radioSelected);
+
+    }
+    /*public void View() {
+        setContentPane(panel1); //Panel1 contains all elements
+
+
+    }*/
 }
