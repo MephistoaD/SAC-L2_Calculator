65954160733c90e4ed84ff21b936c4d5b33b0277
Lcarralde15 <lcarralde27@gmail.com>
2021-12-13 12:53:28 +0100

    Update EthernetCalculator.java
    Falya por completar

--- code/src/main/java/calculators/EthernetCalculator.java
+++ code/src/main/java/calculators/EthernetCalculator.java
@@ -3,7 +3,45 @@ import valueobjects.InputValues;
 
 public class EthernetCalculator implements Calculator{
 
+    int eth_frames;
+    int bTotal;
+    int padding;
+    int bytesframe = 1500;
+
     @Override
     public  OutputValues calculateEthernet(int bytes){
+        int array[] = new int[3];
+
+        if(bytes % bytesframe == 0) {
+            eth_frames =(bytes / bytesframe);
+        } else if (bytes > bytesframe){
+            eth_frames = (bytes / bytesframe) + 1;
+        }else {
+            eth_frames = 1;
+        }
+
+        int rest = bytes % bytesCell;
+
+
+        btotal = eth_frames * totalBytes;
+        array[0] = btotal;
+        array[1] = eth_frames;
+        array[2] = padding;
+
+
+        for(int i=0;i<3;i++) {
+            switch(i) {
+                case 0: System.out.print("total bytes: ");
+                    break;
+                case 1: System.out.print("ethernet frames: ");
+                    break;
+                case 2: System.out.print("padding: ");
+                    break;
+            }
+            System.out.println(array[i]);
+        }
+        return array;
+    }
+
     }
 }
