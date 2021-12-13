44a18d6086e7da0c24f1fe4692994a42ca4dbac3
danilostao <danilostao23@gmail.com>
2021-12-13 18:30:42 +0100

    Create GUIView1.java

---
+++ code/src/main/java/gui/GUIView1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class GUIView1 {
+    private JPanel panel1;
+    private JTextField introduceTheBytesTextField;
+    private JRadioButton AAL34RadioButton;
+    private JRadioButton AAL5RadioButton;
+    private JRadioButton ethernetRadioButton;
+    private JButton runButton;
+    private JCheckBox paddingCheckBox;
+}
