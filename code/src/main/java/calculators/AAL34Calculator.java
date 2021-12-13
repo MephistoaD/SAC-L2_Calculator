bd6effc5ecbc5dd5ee17382ebb9dc88528c27cc7
Lcarralde15 <lcarralde27@gmail.com>
2021-12-13 12:35:44 +0100

    Update AAL34Calculator.java

--- code/src/main/java/calculators/AAL34Calculator.java
+++ code/src/main/java/calculators/AAL34Calculator.java
@@ -2,25 +2,29 @@ package  calculators;
 import valueobjects.InputValues;
 
 public class AAL34Calculator implements Calculator{
+    int numCeldas;
+    int bytesCell = 44;
+    int totalBytes = 48;
 
     @Override
     public OutputValues calculateALL34ATM (int bytes ) {
 
             int array[] = new int[3];
 
-            int numCeldas = bytes / 44;
 
-            if(bytes % 44 == 0) {
-                numCeldas = (bytes / 44);
-            }else if (n > 44){
-                numCeldas = (bytes / 44) + 1;
+            numCeldas = bytes / bytesCell;
+
+            if(bytes % bytesCell == 0) {
+                numCeldas = (bytes / bytesCell);
+            }else if (n > bytesCell){
+                numCeldas = (bytes / bytesCell) + 1;
             }else {
                 numCeldas = 1;
             }
-            int resto = bytes % 44;
-            int padding = 48 - 4 - resto;
+            int resto = bytes % bytesCell;
+            int padding = bytesCell - resto;
 
-            array[0] = numCeldas* 48;
+            array[0] = numCeldas * totalBytes;
             array[1] = numCeldas;
             array[2] = padding;
 
