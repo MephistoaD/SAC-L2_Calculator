aa88fbbd9b6bf9a251f995e755063dc5eba052e1
Lcarralde15 <lcarralde27@gmail.com>
2021-12-14 21:54:54 +0100

    Update AAL5Calculator.java

--- code/src/main/java/calculators/AAL5Calculator.java
+++ code/src/main/java/calculators/AAL5Calculator.java
@@ -1,7 +1,6 @@
-package  calculators;
-import valueobjects.InputValues;
+package code.src.main.java.calculators;
 
-public class AAL5Calculator implements Calculator{
+public class AAL5Calculator implements calculators.Calculator {
     int trailer;
     int numCeldas;
     int total = 0;
@@ -10,48 +9,55 @@ public class AAL5Calculator implements Calculator{
     int aux = 40;
     int totalBytes = 53;
 
+
+
+
+
     @Override
-    public OutputValues calculateALL5ATM(int bytes ) {
+    public int[] calculate(int bytes) {
+            int data[] = new int[3];
 
-        int array[] = new int[3];
-        
-        if(bytes % bytesCell == 0) {
-            numCeldas =(bytes / bytesCell);
-        } else if (bytes > bytesCell){
-            numCeldas = (bytes / bytesCell) + 1;
-        }else {
-            numCeldas = 1;
-        }
-
-        int rest = bytes % bytesCell;
-
-        if(resto < aux) {
-            padding = bytesCell - trailer - rest;
-        } else if (rest == aux ){
-            padding = 0;
-        } else {
-            int num = rest / aux;
-            numCeldas++;
-            int x = resto % aux;
-            padding = aux - x + 8;
-        }
-        total = numCeldas * totalBytes;
-        array[0] = total;
-        array[1] = numCeldas;
-        array[2] = padding;
-
-
-        for(int i=0;i<3;i++) {
-            switch(i) {
-                case 0: System.out.print("total bytes: ");
-                    break;
-                case 1: System.out.print("total cells: ");
-                    break;
-                case 2: System.out.print("padding: ");
-                    break;
+            if (bytes % bytesCell == 0) {
+                numCeldas = (bytes / bytesCell);
+            } else if (bytes > bytesCell) {
+                numCeldas = (bytes / bytesCell) + 1;
+            } else {
+                numCeldas = 1;
             }
-            System.out.println(array[i]);
-        }
-        return array;
+
+            int rest = bytes % bytesCell;
+
+            if (rest < aux) {
+                padding = bytesCell - trailer - rest;
+            } else if (rest == aux) {
+                padding = 0;
+            } else {
+                int num = rest / aux;
+                numCeldas++;
+                int x = rest % aux;
+                padding = aux - x + 8;
+            }
+            total = numCeldas * totalBytes;
+            data[0] = total;
+            data[1] = numCeldas;
+            data[2] = padding;
+
+
+            for (int i = 0; i < 3; i++) {
+                switch (i) {
+                    case 0:
+                        System.out.print("Bytes: ");
+                        break;
+                    case 1:
+                        System.out.print("Cells: ");
+                        break;
+                    case 2:
+                        System.out.print("Padding: ");
+                        break;
+                }
+                System.out.println(data[i]);
+            }
+            return data;
+
     }
 }
