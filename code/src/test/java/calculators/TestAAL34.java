73426d7c790573a1177a6e11b3df07f189501106
Lcarralde15 <lcarralde27@gmail.com>
2021-12-14 21:55:21 +0100

    Update TestAAL34.java

--- code/src/test/java/calculators/TestAAL34.java
+++ code/src/test/java/calculators/TestAAL34.java
@@ -1,5 +1,6 @@
 package calculators;
 
+import code.src.main.java.calculators.AAL34Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import valueobjects.OutputValues;
