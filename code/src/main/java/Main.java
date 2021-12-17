b03e9c0304e89470a2f961d170f679125167dca9
Lcarralde15 <lcarralde27@gmail.com>
2021-12-17 17:39:33 +0100

    Update Main.java

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -1,11 +1,11 @@
 
 
-import calculators.Calculator;
-import cli.CLIInterpreter;
-import gui.GUIMode;
-import valueobjects.InputValues;
-import valueobjects.L2Protocol;
-import valueobjects.OutputValues;
+import code.src.main.java.calculators.Calculator;
+import code.src.main.java.cli.CLIInterpreter;
+import code.src.main.java.gui.GUIMode;
+import code.src.main.java.valueobjects.InputValues;
+import code.src.main.java.valueobjects.L2Protocol;
+import code.src.main.java.valueobjects.OutputValues;
 
 import java.util.Objects;
 
